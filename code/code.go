67ae636295deac021b610a067dad906bbfaf71e1
Soshi Katsuta <soshi.katsuta@gmail.com>
2019-03-27 04:58:44 +0900

    code: add ReadOperands() and ReadUint16() functions and Instructions#String() method

--- code/code.go
+++ code/code.go
@@ -3,10 +3,47 @@ package code
 import (
 	"encoding/binary"
 	"fmt"
+	"strings"
 )
 
 // Instructions represents a sequence of instructions.
 type Instructions []byte
+
+func (insns Instructions) String() string {
+	var out strings.Builder
+
+	i := 0
+	for i < len(insns) {
+		def, err := Lookup(insns[i])
+		if err != nil {
+			fmt.Fprintf(&out, "ERROR: %s\n", err)
+			continue
+		}
+
+		operands, read := ReadOperands(def, insns[i+1:])
+		fmt.Fprintf(&out, "%04d %s\n", i, insns.formatInstruction(def, operands))
+
+		i += 1 + read
+	}
+
+	return out.String()
+}
+
+func (insns Instructions) formatInstruction(def *Definition, operands []int) string {
+	operandCount := len(def.OperandWidths)
+
+	if len(operands) != operandCount {
+		return fmt.Sprintf("ERROR: operand length %d does not match defined %d",
+			len(operands), operandCount)
+	}
+
+	switch operandCount {
+	case 1:
+		return fmt.Sprintf("%s 0x%X", def.Name, operands[0])
+	}
+
+	return fmt.Sprintf("ERROR: unhandled operand width for %s: %d\n", def.Name, operandCount)
+}
 
 // Opcode represents an opcode.
 type Opcode byte
@@ -66,4 +103,26 @@ func Make(op Opcode, operands ...int) []byte {
 	}
 
 	return insn
+}
+
+// ReadOperands reads operands from bytecode instructions based on the definition of an opcode.
+// It returns operands read and the offset describing the starting position of next opcode.
+func ReadOperands(def *Definition, insns Instructions) (operands []int, offset int) {
+	operands = make([]int, len(def.OperandWidths))
+
+	for i, width := range def.OperandWidths {
+		switch width {
+		case 2: // 2 bytes
+			operands[i] = int(ReadUint16(insns[offset:]))
+		}
+
+		offset += width
+	}
+
+	return operands, offset
+}
+
+// ReadUint16 reads a single uint16 value from bytecode instruction sequence.
+func ReadUint16(insns Instructions) uint16 {
+	return binary.BigEndian.Uint16(insns)
 }
--- code/code_test.go
+++ code/code_test.go
@@ -1,6 +1,63 @@
 package code
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
+
+func TestInstructionsString(t *testing.T) {
+	insns := []Instructions{
+		Make(OpConstant, 0x1),
+		Make(OpConstant, 0x2),
+		Make(OpConstant, 0xFF),
+	}
+
+	want := `0000 OpConstant 0x1
+0003 OpConstant 0x2
+0006 OpConstant 0xFF
+`
+
+	concat := make(Instructions, 0, len(insns))
+	for _, ins := range insns {
+		concat = append(concat, ins...)
+	}
+
+	fmt.Println(concat)
+	got := concat.String()
+	if got != want {
+		t.Errorf("instructions wrongly formatted.\nwant:\n%s\ngot:\n%s", want, got)
+	}
+}
+
+func TestReadOperands(t *testing.T) {
+	tests := []struct {
+		op        Opcode
+		operands  []int
+		bytesRead int
+	}{
+		{op: OpConstant, operands: []int{0xFF}, bytesRead: 2},
+	}
+
+	for _, tt := range tests {
+		b := tt.op.Byte()
+		def, err := Lookup(b)
+		if err != nil {
+			t.Fatalf("definition for byte %x not found: %s", b, err)
+		}
+
+		insns := Make(tt.op, tt.operands...)
+		operandsRead, n := ReadOperands(def, insns[1:])
+		if n != tt.bytesRead {
+			t.Fatalf("number of bytes read wrong. want=%d, got=%d", tt.bytesRead, n)
+		}
+
+		for i, want := range tt.operands {
+			if operandsRead[i] != want {
+				t.Errorf("operand wrong. want=%d, got=%d", want, operandsRead[i])
+			}
+		}
+	}
+}
 
 func TestMake(t *testing.T) {
 	tests := []struct {
