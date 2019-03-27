b650ea2df3eb6449059fff98ec45c38a8f542de9
Soshi Katsuta <soshi.katsuta@gmail.com>
2019-03-28 01:22:14 +0900

    code: move type definition of Opcode and Definition to the top of code.go

--- code/code.go
+++ code/code.go
@@ -6,6 +6,38 @@ import (
 	"strings"
 )
 
+// Opcode represents an opcode.
+type Opcode byte
+
+// Byte returns the corresponding byte of the opcode `op`.
+func (op Opcode) Byte() byte {
+	return byte(op)
+}
+
+const (
+	// OpConstant represents an opcode which pushes a constant value on to the stack.
+	OpConstant Opcode = iota
+)
+
+// Definition represents the definition of an opcode.
+type Definition struct {
+	Name          string
+	OperandWidths []int
+}
+
+var definitions = map[Opcode]*Definition{
+	OpConstant: {Name: "OpConstant", OperandWidths: []int{2}},
+}
+
+// Lookup performs a lookup for `op` in the definitions of opcodes.
+func Lookup(op byte) (*Definition, error) {
+	def, ok := definitions[Opcode(op)]
+	if !ok {
+		return nil, fmt.Errorf("opcode %d undefined", op)
+	}
+	return def, nil
+}
+
 // Instructions represents a sequence of instructions.
 type Instructions []byte
 
@@ -43,38 +75,6 @@ func (insns Instructions) formatInstruction(def *Definition, operands []int) str
 	}
 
 	return fmt.Sprintf("ERROR: unhandled operand width for %s: %d\n", def.Name, operandCount)
-}
-
-// Opcode represents an opcode.
-type Opcode byte
-
-// Byte returns the corresponding byte of the opcode `op`.
-func (op Opcode) Byte() byte {
-	return byte(op)
-}
-
-const (
-	// OpConstant represents an opcode which pushes a constant value on to the stack.
-	OpConstant Opcode = iota
-)
-
-// Definition represents the definition of an opcode.
-type Definition struct {
-	Name          string
-	OperandWidths []int
-}
-
-var definitions = map[Opcode]*Definition{
-	OpConstant: {Name: "OpConstant", OperandWidths: []int{2}},
-}
-
-// Lookup performs a lookup for `op` in the definitions of opcodes.
-func Lookup(op byte) (*Definition, error) {
-	def, ok := definitions[Opcode(op)]
-	if !ok {
-		return nil, fmt.Errorf("opcode %d undefined", op)
-	}
-	return def, nil
 }
 
 // Make makes a bytecode instruction sequence from an opcode and operands.
