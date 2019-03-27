c690963ae8eb670f78862edc59afb00267df3d30
Soshi Katsuta <soshi.katsuta@gmail.com>
2019-03-28 01:12:55 +0900

    vm: add VM struct and make it interprets OpConstant instructions

---
+++ vm/vm.go
@@ -0,0 +1,76 @@
+package vm
+
+import (
+	"errors"
+
+	"github.com/skatsuta/monkey-compiler/code"
+	"github.com/skatsuta/monkey-compiler/compiler"
+	"github.com/skatsuta/monkey-compiler/object"
+)
+
+// StackSize is an initial stack size.
+const StackSize = 2048
+
+// VM is a virtual machine which interprets and executes bytecode instructions.
+type VM struct {
+	consts []object.Object
+	insns  code.Instructions
+
+	stack []object.Object
+	// Stackpointer always points to the *next* value. Top of stack is `stack[sp-1]`.
+	sp int
+}
+
+// New creates a new VM instance which executes the given bytecode.
+func New(bytecode *compiler.Bytecode) *VM {
+	return &VM{
+		insns:  bytecode.Instructions,
+		consts: bytecode.Constants,
+
+		stack: make([]object.Object, StackSize),
+		sp:    0,
+	}
+}
+
+// StackTop returns an object at the top of stack.
+func (vm *VM) StackTop() object.Object {
+	if vm.sp == 0 {
+		return nil
+	}
+	return vm.stack[vm.sp-1]
+}
+
+// Run executes bytecode instructions.
+func (vm *VM) Run() error {
+	for ip := 0; ip < len(vm.insns); ip++ {
+		op := code.Opcode(vm.insns[ip])
+
+		switch op {
+		case code.OpConstant:
+			// Read a 2-byte operand from the next position
+			constIdx := code.ReadUint16(vm.insns[ip+1:])
+			// Because the operand is 2-byte width and we already read it,
+			// increment the pointer by 2 (bytes)
+			ip += 2
+
+			if err := vm.push(vm.consts[constIdx]); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func (vm *VM) push(obj object.Object) error {
+	if vm.sp >= StackSize {
+		return errors.New("stack overflow")
+	}
+
+	// Push the object to the top of stack
+	vm.stack[vm.sp] = obj
+	// Increment the stack pointer
+	vm.sp++
+
+	return nil
+}
---
+++ vm/vm_test.go
@@ -0,0 +1,77 @@
+package vm
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/skatsuta/monkey-compiler/ast"
+	"github.com/skatsuta/monkey-compiler/compiler"
+	"github.com/skatsuta/monkey-compiler/lexer"
+	"github.com/skatsuta/monkey-compiler/object"
+	"github.com/skatsuta/monkey-compiler/parser"
+)
+
+func TestIntegerArithmetic(t *testing.T) {
+	tests := []vmTestCase{
+		{"1", 1},
+		{"2", 2},
+		{"1 + 2", 2}, // FIXME
+	}
+
+	runVMTests(t, tests)
+}
+
+type vmTestCase struct {
+	input string
+	want  interface{}
+}
+
+func runVMTests(t *testing.T, tests []vmTestCase) {
+	t.Helper()
+
+	for _, tt := range tests {
+		program := parse(tt.input)
+
+		complr := compiler.New()
+		if err := complr.Compile(program); err != nil {
+			t.Fatalf("compiler error: %s", err)
+		}
+
+		vm := New(complr.Bytecode())
+		if err := vm.Run(); err != nil {
+			t.Fatalf("vm error: %s", err)
+		}
+
+		got := vm.StackTop()
+
+		testExpectedObject(t, tt.want, got)
+	}
+}
+
+func parse(input string) *ast.Program {
+	return parser.New(lexer.New(input)).ParseProgram()
+}
+
+func testExpectedObject(t *testing.T, want interface{}, got object.Object) {
+	t.Helper()
+
+	switch want := want.(type) {
+	case int:
+		if err := testIntegerObject(int64(want), got); err != nil {
+			t.Errorf("testIntegerObject failed: %s", err)
+		}
+	}
+}
+
+func testIntegerObject(want int64, got object.Object) error {
+	result, ok := got.(*object.Integer)
+	if !ok {
+		return fmt.Errorf("object is not Integer. got=%T (%#v)", got, got)
+	}
+
+	if result.Value != want {
+		return fmt.Errorf("object has wrong value. want=%d, got=%d", want, result.Value)
+	}
+
+	return nil
+}
